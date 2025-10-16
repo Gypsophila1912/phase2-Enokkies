1cf4e7c00cd09be8b04c7fb670d8212c5bd46744
Gypsophila1912 <noma.19.anko@gmail.com>
2025-10-16 16:06:11 +0900

    add リレーションの追加

--- app/Models/Groups_user.php
+++ app/Models/Groups_user.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Groups_user extends Model
+class GroupUser extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'groups_users';
+    protected $fillable = [
+        'group_id',
+        'title',
+        'description',
+        'completed_by',
+        'completed_at',
+        'points',
+    ];
+
+    //グループ
+    public function group()
+    {
+        return $this->belongsTo(Group::class);
+    }
+
+    //ユーザー
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'completed_by');
+    }
 }
